baafc7570f6a5592f3a7f70f4cbe93ea8e906739
Saher Wasim <saherwasim1@gmail.com>
2017-10-30 22:41:20 -0700

    Update stack.js

--- stack.js
+++ stack.js
@@ -23,3 +23,24 @@ dogList.push("Romeo");
 dogList.push("Simba");
 
 console.log(dogList);
+
+
+
+
+
+-------- OR --------------
+var myStack = [];
+
+//push
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+
+//pop
+myStack.pop(); //3
+myStack.pop(); //2
+myStack.pop(); //1  
+  
+  
+  
+  
